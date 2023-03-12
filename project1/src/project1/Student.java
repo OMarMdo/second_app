61728823b465a8e66a2533899344dceedbfe44e9
OMarMdo <120013869+OMarMdo@users.noreply.github.com>
2023-03-12 10:09:44 +0600

    First_project

---
+++ project1/src/project1/Student.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project1;
+
+/**
+ *
+ * @author OMAR
+ */
+public class Student {
+    private String name;
+    private int roll;
+
+    public Student(String name, int roll) {
+        this.name = name;
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+    
+    
+}
---
+++ project1/src/project1/test.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project1;
+
+/**
+ *
+ * @author OMAR
+ */
+public class test {
+    public static void main(String args[])
+    {
+        Student s1= new Student("Omar",26);
+        s1.setName("Nibir");
+        System.out.println(s1.getName());
+        
+        s1.setRoll(43);
+         System.out.println(s1.getRoll());
+        
+    }
+}
